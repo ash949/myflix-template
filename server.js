75c5bb8182f54fda1df798eeb1b595da8416d8ab
Hamza Ashour <hamza.ashour.work@gmail.com>
2019-11-19 22:03:28 +0200

    3.3

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ app.get('/', function (req, res) {
 
 
 // Return a list of ALL movies to the user
-app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
+app.get("/movies", function (req, res) {
+  // app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies);
